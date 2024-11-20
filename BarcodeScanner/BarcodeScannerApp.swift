cb075bdfe0a5f99a5d4a7cb66e48037070bb029c
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-11-20 23:31:42 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ahmet Bostancıklıoğlu on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
